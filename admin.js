0887b2e346ccfe0e5b424dd44935a389bb62cb43
elsayedhasaneisa1-collab <elsayedhasaneisa1@gmail.com>
2026-09-20 20:48:53 +0300

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,65 @@
+const siteData = {
+  site: {
+    name: "Team Cyber Force",
+    description: "فريق سايبر فورث يقدم احدث اصداراته",
+    logo: "logo.png",
+    color: "#00a3ff"
+  },
+
+  services: [
+    {
+      icon: "💻",
+      title: "تطوير المواقع",
+      description: "تصميم وتطوير مواقع احترافية متجاوبة مع جميع الأجهزة"
+    },
+    {
+      icon: "🎓",
+      title: "تدريب وشروحات",
+      description: "كورسات وشرحات مبسطة في مختلف مجالات التقنية"
+    },
+    {
+      icon: "🛡️",
+      title: "الأمن السيبراني",
+      description: "حماية المواقع والأنظمة من الاختراقات والثغرات"
+    },
+    {
+      icon: "📱",
+      title: "تصميم واجهات",
+      description: "تصميم واجهات مستخدم عصرية وجذابة"
+    }
+  ],
+
+  team: [
+    {
+      name: "EL-sayed Hasan",
+      role: "مطور ويب",
+      image: "sayed.png",
+      whatsapp: "201128351702"
+    },
+    {
+      name: "Mohamed Emad",
+      role: "مدرب",
+      image: "mo.png",
+      whatsapp: "201123058983"
+    } , 
+    {
+      name: "Mero",
+      role: "مديره اعمال",
+      image: "mero.png",
+      whatsapp: "201128996872"
+    }
+  ],
+
+  projects: [
+    {
+      title: "منصه Cyber force ",
+      description: "منصه تعليميه تقدم شروحات في مختلف المجالات",
+      link: "https://elsayedhasaneisa1-collab.github.io/SAYODA-CYBER-FORCE/login.html"
+    }
+  ],
+
+  contact: {
+    email: "elsayedhasaneisa1@gmail.com",
+    whatsapp: "201041254010"
+  }
+};
